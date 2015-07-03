d1fb757433ecf6b8a3126b13e3fab8ae69538c43
Ayrat Aminev <airato.a@gmail.com>
2015-07-03 16:07:43 +0300

    explicitly save session before redirect
    Prevent missing and leaking session data
    https://github.com/expressjs/session/issues/167#issuecomment-107091763

--- routes/index.js
+++ routes/index.js
@@ -12,14 +12,19 @@ router.get('/register', function(req, res) {
     res.render('register', { });
 });
 
-router.post('/register', function(req, res) {
+router.post('/register', function(req, res, next) {
     Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
         if (err) {
           return res.render("register", {info: "Sorry. That username already exists. Try again."});
         }
 
         passport.authenticate('local')(req, res, function () {
-            res.redirect('/');
+            req.session.save(function (err) {
+                if (err) {
+                    return next(err);
+                }
+                res.redirect('/');
+            });
         });
     });
 });
@@ -29,13 +34,23 @@ router.get('/login', function(req, res) {
     res.render('login', { user : req.user });
 });
 
-router.post('/login', passport.authenticate('local'), function(req, res) {
-    res.redirect('/');
+router.post('/login', passport.authenticate('local'), function(req, res, next) {
+    req.session.save(function (err) {
+        if (err) {
+            return next(err);
+        }
+        res.redirect('/');
+    });
 });
 
-router.get('/logout', function(req, res) {
+router.get('/logout', function(req, res, next) {
     req.logout();
-    res.redirect('/');
+    req.session.save(function (err) {
+        if (err) {
+            return next(err);
+        }
+        res.redirect('/');
+    });
 });
 
 router.get('/ping', function(req, res){
