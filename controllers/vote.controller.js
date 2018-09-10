4fdc11ca3999df26e6c86d8f254961b5517dcefc
SsangWoo <tkddn204@gmail.com>
2018-09-10 16:30:05 +0900

    투표를 이미 눌렀던 적이 있을 경우 추가

--- controllers/vote.controller.js
+++ controllers/vote.controller.js
@@ -9,12 +9,12 @@ const hec = require('../hec/hec.api');
 const ipfs = require('../ipfs/ipfs');
 
 const mkdirSync = function (dirPath) {
-  try {
-    fs.mkdirSync(dirPath)
-  } catch (err) {
-    if (err.code !== 'EEXIST') throw err
-  }
-}
+    try {
+        fs.mkdirSync(dirPath)
+    } catch (err) {
+        if (err.code !== 'EEXIST') throw err
+    }
+};
 
 module.exports = {
     getVote: async (req, res) => {
@@ -31,17 +31,27 @@ module.exports = {
             if (voterState !== "Voted") {
                 electionDetail.candidateList = await candidateApi.getCandidateList(electionAddress);
 
+                if (!fs.existsSync(path.resolve(`./hec/data/candidate/${electionAddress}`))) {
+                    const candidateListAtFile = fs.readFileSync(
+                        `./hec/data/candidate/${electionAddress}/${voterAddress}`);
+                    const candidateList = candidateListAtFile.slice(',');
+                    return res.render('election/vote', {
+                        electionDetail: electionDetail,
+                        candidateList: candidateList,
+                        path: req.path
+                    });
+                }
                 const total = electionDetail.candidateList.length;
                 await hec.encryptCandidateList(electionAddress, voterAddress,
                     total, 'hec/data', (out, err) => {
-                        if(err) console.error(err);
+                        if (err) console.error(err);
                         console.log(out);
 
                         // fileList 만들고
                         let fileList = [];
                         for (let i = 0; i < total; i++) {
                             const path = `/home/ssangwoo/prototype/hec/data/candidate/${electionAddress.toLowerCase()}-${i}-${voterAddress.toLowerCase()}.txt`;
-			    console.log(path);
+                            console.log(path);
                             fileList.push({
                                 path: path,
                                 content: new Buffer.from(path)
@@ -50,45 +60,29 @@ module.exports = {
 
                         // IPFS에 저장
                         return ipfs.files.add(fileList, (err, files) => {
-			    if(err) {
-				console.error(err);
-				return res.send('error');
-			    }
+                            if (err) {
+                                console.error(err);
+                                return res.send('error');
+                            }
                             console.log(files);
-			    let candidateList = [];
-			    for(let i = 0; i < total; i++) {
-				candidateList[i] = files[i].hash;
-			    }
-			    console.log("aaa : " + candidateList.toString());
+                            let candidateList = [];
+                            for (let i = 0; i < total; i++) {
+                                candidateList[i] = files[i].hash;
+                            }
+                            console.log("aaa : " + candidateList.toString());
 
                             // 파일 저장
-			    mkdirSync(path.resolve(`./hec/data/candidate/${electionAddress}`));
+                            mkdirSync(path.resolve(`./hec/data/candidate/${electionAddress}`));
                             fs.writeFileSync(
                                 `./hec/data/candidate/${electionAddress}/${voterAddress}`, candidateList.toString());
 
-                            res.render('election/vote', {
+                            return res.render('election/vote', {
                                 electionDetail: electionDetail,
                                 candidateList: candidateList,
                                 path: req.path
                             });
                         });
                     });
-                // 후보자 hash 리스트
-                // await hecIpfsApi.makeEncryptCandidateList(
-                //     electionAddress,
-                //     voterAddress,
-                //     electionDetail.candidateList.length,
-                //     '/home/ssangwoo/prototype/hec/data', (err, out) => {
-                //         if(err) console.log(err);
-                //         console.log(out);
-                //
-                //     const content = fs.readFileSync(`./hec/data/candidate/${electionAddress}/${voterAddress}`);
-                //     electionDetail.candidateHashList = content.slice(',');
-                //
-                //      res.render('election/vote', {
-                //         	electionDetail: electionDetail,
-                //        		path: req.path
-                // 	});
             } else {
                 res.redirect(req.path.substring(0, req.path.length - 5));
             }
@@ -96,36 +90,47 @@ module.exports = {
             res.send(err.toString());
         }
     },
-    postVote: async (req, res) => {
+    postVote:
+        async (req, res) => {
 
-        if (!req.user) {
-            res.redirect('/login');
-        }
-
-        try {
-            const electionAddress = req.params.address;
-            const voterAddress = req.user.etherAccount;
-            const candidate = req.body.candidate;
-
-            let voterState = await voterApi.getVoterState(electionAddress, voterAddress);
-            if (voterState !== "Voted") {
-                await electionApi.vote(electionAddress, voterAddress, candidate);
-                voterState = await voterApi.getVoterState(electionAddress, voterAddress);
-                if (voterState === "Voted") {
-                    Account.findOneAndUpdate({username: req.user.username}, {
-                        $push: {votingVotes: electionAddress}
-                    }, {upsert: true}, (err, data) => {
-                        if (err) res.send(err.toString());
-                        else res.redirect(req.path.substring(0, req.path.length - 5));
-                    });
-                } else {
-                    res.send('투표 실패');
-                }
-            } else {
-                alert("이미 참여하신 투표입니다.");
+            if (!req.user) {
+                res.redirect('/login');
             }
-        } catch (err) {
-            res.send(err.toString());
+
+            try {
+                const electionAddress = req.params.address;
+                const voterAddress = req.user.etherAccount;
+                const candidate = req.body.candidate;
+
+                // 현재 투표 상태
+                let voterState = await voterApi.getVoterState(electionAddress, voterAddress);
+                if (voterState !== "Voted") {
+                    // 투표완료 상태가 아니면
+
+                    // 컨트렉트에 투표 요청을 보냄
+                    await electionApi.vote(electionAddress, voterAddress, candidate);
+
+                    // 다시 투표 상태를 확인하여
+                    voterState = await voterApi.getVoterState(electionAddress, voterAddress);
+                    if (voterState === "Voted") {
+                        // 투표 완료 상태가 되면
+
+                        // DB 업데이트
+                        Account.findOneAndUpdate({username: req.user.username}, {
+                            $push: {votingVotes: electionAddress}
+                        }, {upsert: true}, (err, data) => {
+                            if (err) res.send(err.toString());
+                            else res.redirect(req.path.substring(0, req.path.length - 5));
+                        });
+                    } else {
+                        res.send('투표 실패');
+                    }
+                } else {
+                    alert("이미 참여하신 투표입니다.");
+                }
+            } catch (err) {
+                res.send(err.toString());
+            }
         }
-    }
-};
+}
+;
